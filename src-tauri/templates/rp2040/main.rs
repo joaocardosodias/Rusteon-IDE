6db4d7841f4793df11c90a5577c142b2095fcffc
joaocardosodias <joao.dias@sou.inteli.edu.br>
2026-04-12 23:11:31 -0300

    chore: update HAL and runtime dependencies and apply unsafe attribute to bootloader section

--- src-tauri/templates/rp2040/main.rs
+++ src-tauri/templates/rp2040/main.rs
@@ -6,7 +6,7 @@ use hal::pac;
 use cortex_m_rt::entry;
 use panic_halt as _;
 
-#[link_section = ".boot2"]
+#[unsafe(link_section = ".boot2")]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 
